40e2066406dcf293c8dca019513166ae13725a94
Lightgazer <lightgazer@tuta.io>
2021-04-13 21:35:10 +1000

    Добавьте файлы проекта.

---
+++ NotPong/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NotPong
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
